242ab4fda46ff7cbcf765dc0143c824f7ebb0827
Zhen Qian <xhdp123@126.com>
2018-07-20 17:03:27 +0800

    first submit

--- nhjs/urls.py
+++ nhjs/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from nhjsWeb.views import *
+from django.views.generic.base import TemplateView
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nhjs.views.home', name='home'),
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
                        url(r'^homepage/', home),
                        url('^$', home),
+                       url(r'^QiYeJianJie/', TemplateView.as_view(template_name="QiYeJianJie.html")),
 )
